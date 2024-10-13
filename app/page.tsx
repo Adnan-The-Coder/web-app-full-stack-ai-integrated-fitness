8e79b8775044e93025aad9ac58d75df591b033c3
Adnan <adnanali23189@gmail.com>
2024-10-14 01:02:26 +0530

    ExperienceHealth.tsx component added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ExperienceHealth from "@/components/ExperienceHealth";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
@@ -7,6 +8,7 @@ export default function Home() {
     <>
     <Navbar/>
     <Hero/>
+    <ExperienceHealth/>
     <Footer/>
     <Footer/>
     <Footer/>
